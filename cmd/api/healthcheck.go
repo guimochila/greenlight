d6f83e162370560924d0dafb4596d475c3db48af
Guilherme <gui.mochila@gmail.com>
2024-12-17 21:07:17 +0100

    feat(core): initial commit

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,22 @@
+// Copyleft (c) 2024, guimochila. Happy Coding.
+package main
+
+import (
+	"net/http"
+)
+
+func (app *application) healthcheckHandler(w http.ResponseWriter, _ *http.Request) {
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+
+		return
+	}
+}
---
+++ cmd/api/movies.go
@@ -0,0 +1,23 @@
+// Copyleft (c) 2024, guimochila. Happy Coding.
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create a movie")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+
+		return
+	}
+
+	fmt.Fprintf(w, "show the details of movie %d\n", id)
+}
