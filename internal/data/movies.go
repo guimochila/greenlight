cfcccb72627bba5e15ae5db92c93252fc4d6e104
Guilherme <gui.mochila@gmail.com>
2024-12-19 13:35:37 +0100

    feat(validator): add validation json

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,7 +1,11 @@
 // Copyleft (c) 2024, guimochila. Happy Coding.
 package data
 
-import "time"
+import (
+	"time"
+
+	"github.com/guimochila/greenlight/internal/validator"
+)
 
 type Movie struct {
 	ID        int64     `json:"id"`
@@ -11,4 +15,27 @@ type Movie struct {
 	Runtime   Runtime   `json:"runtime,omitempty"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
+}
+
+const (
+	TitleBytesLong = 500
+	MinimunYear    = 1888
+	MaxGenres      = 5
+)
+
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= TitleBytesLong, "title", "must not be more than 500 bytes long")
+
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= MinimunYear, "year", "must be greater than 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= MaxGenres, "genres", "must not contain more than 5 genres")
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
 }
